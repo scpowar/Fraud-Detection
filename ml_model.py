6cf70ac6800ff84422de4412a7bf83ccb77370a5
scpowar <scpowar@gmail.com>
2021-12-28 16:31:56 +0530

    added 3 models

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,11 @@
 print('adding ml related code here')
+
+def model1():
+    print('adding code for ml model1')
+
+def model2():
+    print('adding code for ml model1')
+
+def model3():
+    print('adding code for ml model1')
+
